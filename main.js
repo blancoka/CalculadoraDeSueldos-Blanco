ec379606f90a0afed7873407b87ee23650bb3b9d
Kassia Blanco <kassiablanco@192.168.1.13>
2023-02-26 17:07:52 -0300

    second commit

--- main.js
+++ main.js
@@ -1,72 +1,81 @@
-/** Valor BPC 2022. */
-const BPC = 5164;
 
-const Tope1 = 7 * BPC;
-const Tope2 = 10 * BPC;
-const Tope3 = 15 * BPC;
-const Tope4 = 30 * BPC;
-const Tope5 = 50 * BPC;
-const Tope6 = 75 * BPC;
-const Tope7 = 115 * BPC;
+/** DATA ENTRY  */
+function solicitarSueldoNominal() {
+    return prompt (`Ingrese su sueldo nominal`)
+} 
 
-const tasa7 = 36;
-const tasa6 = 31;
-const tasa5 = 27;
-const tasa4 = 25;
-const tasa3 = 24;
-const tasa2 = 15;
-const tasa1 = 10;
+/** CALCULATIONS */
 
+function calculoSueldoLiquido(sueldoNominal){
+    /** Valor BPC 2022. */
+    const BPC = 5164;
 
-const APORTES_JUBILATORIOS = 15;
-const APORTES_FONASA_HASTA25BPC = 3;
-const APORTES_FONASA_DESDE25BPC = 4.5;
-const APORTE_FRL = 0.1;
-const TASA_DEDUCCIONES_HASTA15BPC = 10;
-const TASA_DEDUCCIONES_DESDE15BPC = 8;
+    const TOPE1 = 7 * BPC;
+    const TOPE2 = 10 * BPC;
+    const TOPE3 = 15 * BPC;
+    const TOPE4 = 30 * BPC;
+    const TOPE5 = 50 * BPC;
+    const TOPE6 = 75 * BPC;
+    const TOPE7 = 115 * BPC;
 
-var IRPF = 0;
-var APFONASA = 0;
+    const TASA7 = 36;
+    const TASA6 = 31;
+    const TASA5 = 27;
+    const TASA4 = 25;
+    const TASA3 = 24;
+    const TASA2 = 15;
+    const TASA1 = 10;
 
+    const APORTES_JUBILATORIOS = 15;
+    const APORTES_FONASA_HASTA25BPC = 3;
+    const APORTES_FONASA_DESDE25BPC = 4.5;
+    const APORTE_FRL = 0.1;
+    const TASA_DEDUCCIONES_HASTA15BPC = 10;
+    const TASA_DEDUCCIONES_DESDE15BPC = 8;
 
-let sueldoNominal = prompt (`Ingresar sueldo nominal`)
-
-switch (true) {
-    case sueldoNominal > Tope7 :
-        IRPF = ((sueldoNominal - Tope7) * tasa7 ) / 100;
-        break;
-    case sueldoNominal > Tope6 :
-       IRPF = ((sueldoNominal - Tope6) * tasa6 ) / 100;
-        break;
-    case sueldoNominal > Tope5 :
-        IRPF = ((sueldoNominal - Tope5) * tasa5 ) / 100;
-        break;
-    case sueldoNominal > Tope4 :
-        IRPF = ((sueldoNominal - Tope4) * tasa4 ) / 100;
-        break;
-    case sueldoNominal > Tope3 :
-        IRPF = ((sueldoNominal - Tope3) * tasa3 ) / 100;
-        break;
-    case sueldoNominal > Tope2 :
-        IRPF = ((sueldoNominal - Tope2) * tasa2 ) / 100;
-        break;
-    case sueldoNominal > Tope1 :
-        IRPF = ((sueldoNominal - Tope1) * tasa1 ) / 100;
-        break;
-    default:
-        IRPF = 0
-        break;
+    var irpf = 0;
+    var apfonasa = 0;
+    var ap_jub = (sueldoNominal * APORTES_JUBILATORIOS) / 100
+    var ap_frl = (sueldoNominal * APORTE_FRL) / 100
+    
+    switch (true) {
+        case sueldoNominal > TOPE7 :
+            irpf = ((sueldoNominal - TOPE7) * TASA7 ) / 100;
+            break;
+        case sueldoNominal > TOPE6 :
+           irpf = ((sueldoNominal - TOPE6) * TASA6 ) / 100;
+            break;
+        case sueldoNominal > TOPE5 :
+            irpf = ((sueldoNominal - TOPE5) * TASA5 ) / 100;
+            break;
+        case sueldoNominal > TOPE4 :
+            irpf = ((sueldoNominal - TOPE4) * TASA4 ) / 100;
+            break;
+        case sueldoNominal > TOPE3 :
+            irpf = ((sueldoNominal - TOPE3) * TASA3 ) / 100;
+            break;
+        case sueldoNominal > TOPE2 :
+            irpf = ((sueldoNominal - TOPE2) * TASA2 ) / 100;
+            break;
+        case sueldoNominal > TOPE1 :
+            irpf = ((sueldoNominal - TOPE1) * TASA1 ) / 100;
+            break;
+        default:
+            irpf = 0
+            break;
+    }
+    if (sueldoNominal > 25 * BPC) apfonasa = (sueldoNominal * APORTES_FONASA_DESDE25BPC) / 100
+    else apfonasa = (sueldoNominal * APORTES_FONASA_HASTA25BPC) / 100
+    if (sueldoNominal > 15 * BPC) irpf = (irpf * TASA_DEDUCCIONES_DESDE15BPC) / 100
+    else irpf = (irpf * TASA_DEDUCCIONES_HASTA15BPC) / 100
+    return (Math.round (sueldoNominal - irpf - ap_jub - ap_frl - apfonasa))
 }
 
-var AP_JUB = (sueldoNominal * APORTES_JUBILATORIOS) / 100
-var AP_FRL = (sueldoNominal * APORTE_FRL) / 100
+function mostrarSueldoLiquido(sueldoLiquido) {
+    alert ("Su sueldo liquido es de $" + sueldoLiquido)
+}
 
-
-if (sueldoNominal > 25 * BPC) APFONASA = (sueldoNominal * 4.5) / 100
-else APFONASA = (sueldoNominal * 3) / 100
-
-if (sueldoNominal > 15 * BPC) IRPF = (IRPF * 8) / 100
-else IRPF = (IRPF * 10) / 100
-
-alert (Math.round (sueldoNominal - IRPF - AP_JUB - AP_FRL - APFONASA))
-
+/** MAIN */
+var sueldoNominal = solicitarSueldoNominal()
+var sueldoLiquido = calculoSueldoLiquido(sueldoNominal)
+mostrarSueldoLiquido(sueldoLiquido)
