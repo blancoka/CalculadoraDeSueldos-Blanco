1ee48fe91b67fd654763f66b159384748a0d8a6a
Kassia Blanco <kassiablanco@192.168.1.13>
2023-03-08 10:49:19 -0300

    second commit

--- main.js
+++ main.js
@@ -1,79 +1,98 @@
 
-/** DATA ENTRY  */
+/** DATA ENTRY */
 function solicitarSueldoNominal() {
     return prompt (`Ingrese su sueldo nominal`)
 } 
+
+class User{
+    constructor (sueldoNominal, hijos, conyugue, hijoscondiscapacidad, hijossindiscapacidad, fondosolidaridad, cajanotarial){
+    this.sueldoNominal = sueldoNominal; 
+    this.hijos = hijos; 
+    this.conyugue = conyugue;
+    this.hijoscondiscapacidad = hijoscondiscapacidad;
+    this.hijossindiscapacidad = hijossindiscapacidad;
+    this.fondosolidaridad = fondosolidaridad;
+    this.cajanotarial = cajanotarial;
+    }
+}
+
+var sueldoNominal = document.getElementById("sueldoNominal")
+var hijosAcargo = document.getElementById("hijosAcargo")
+var conyugueAcargo = document.getElementById("parejaAcargo")
+var hijosCondiscapacidad = document.getElementById("hijosdiscapacitados")
+var hijosSindiscapacidad = document.getElementById("hijosnodiscapacitados")
+var fondodesolid = document.getElementById("fondodesolid")
+var cajaNotarial = document.getElementById("cajaNotarial")
+
+var Usuario = new User(sueldoNominal, hijosAcargo, conyugueAcargo, hijosCondiscapacidad, hijosSindiscapacidad, fondodesolid, cajaNotarial)
 
 /** CALCULATIONS */
 
 function calculoSueldoLiquido(sueldoNominal){
     /** Valor BPC 2022. */
-    const BPC = 5164;
+    const bpc = 5164;
+    const aporte_jubilatorio = 15;
+    const aporte_fonasa_hasta25bpc = 3;
+    const aporte_fonasa_desde25bpc = 4.5;
+    const aporte_frl = 0.1;
+    const tasa_deducciones_hasta15bpc = 10;
+    const tasa_deducciones_desde15bpc = 8;
 
-    const TOPE1 = 7 * BPC;
-    const TOPE2 = 10 * BPC;
-    const TOPE3 = 15 * BPC;
-    const TOPE4 = 30 * BPC;
-    const TOPE5 = 50 * BPC;
-    const TOPE6 = 75 * BPC;
-    const TOPE7 = 115 * BPC;
+    var descuento_fonasa = 0;
+    var descuento_jubilatorio = (sueldoNominal * aporte_jubilatorio) / 100
+    var descuento_frl = (sueldoNominal * aporte_frl) / 100
 
-    const TASA7 = 36;
-    const TASA6 = 31;
-    const TASA5 = 27;
-    const TASA4 = 25;
-    const TASA3 = 24;
-    const TASA2 = 15;
-    const TASA1 = 10;
+    class Franja {
+        constructor(min, max, aporteirpf){
+            this.min = min;
+            this.max = max;
+            this.aporteirpf = aporteirpf;
+        }
 
-    const APORTES_JUBILATORIOS = 15;
-    const APORTES_FONASA_HASTA25BPC = 3;
-    const APORTES_FONASA_DESDE25BPC = 4.5;
-    const APORTE_FRL = 0.1;
-    const TASA_DEDUCCIONES_HASTA15BPC = 10;
-    const TASA_DEDUCCIONES_DESDE15BPC = 8;
-
-    var irpf = 0;
-    var apfonasa = 0;
-    var ap_jub = (sueldoNominal * APORTES_JUBILATORIOS) / 100
-    var ap_frl = (sueldoNominal * APORTE_FRL) / 100
-    
-    switch (true) {
-        case sueldoNominal > TOPE7 :
-            irpf = ((sueldoNominal - TOPE7) * TASA7 ) / 100;
-            break;
-        case sueldoNominal > TOPE6 :
-           irpf = ((sueldoNominal - TOPE6) * TASA6 ) / 100;
-            break;
-        case sueldoNominal > TOPE5 :
-            irpf = ((sueldoNominal - TOPE5) * TASA5 ) / 100;
-            break;
-        case sueldoNominal > TOPE4 :
-            irpf = ((sueldoNominal - TOPE4) * TASA4 ) / 100;
-            break;
-        case sueldoNominal > TOPE3 :
-            irpf = ((sueldoNominal - TOPE3) * TASA3 ) / 100;
-            break;
-        case sueldoNominal > TOPE2 :
-            irpf = ((sueldoNominal - TOPE2) * TASA2 ) / 100;
-            break;
-        case sueldoNominal > TOPE1 :
-            irpf = ((sueldoNominal - TOPE1) * TASA1 ) / 100;
-            break;
-        default:
-            irpf = 0
-            break;
+        pertenece(sueldoNominal){
+            if ((sueldoNominal > this.min) && (sueldoNominal < this.max) )
+            return true
+            else return false
+        }
     }
-    if (sueldoNominal > 25 * BPC) apfonasa = (sueldoNominal * APORTES_FONASA_DESDE25BPC) / 100
-    else apfonasa = (sueldoNominal * APORTES_FONASA_HASTA25BPC) / 100
-    if (sueldoNominal > 15 * BPC) irpf = (irpf * TASA_DEDUCCIONES_DESDE15BPC) / 100
-    else irpf = (irpf * TASA_DEDUCCIONES_HASTA15BPC) / 100
-    return (Math.round (sueldoNominal - irpf - ap_jub - ap_frl - apfonasa))
+    
+    class IRPF{
+        constructor(franjas){
+            this.franjas = franjas
+        }
+    
+        calculodescuentoIRPF(sueldoNominal) {
+            var franja = franjas.find(f => f.pertenece(sueldoNominal)); //me devuelve la franja que cumple que franja.min < sueldoNominal < franja.max
+            var descuento = (franja.aporteirpf * sueldoNominal) / 100; //calculo la plata que se le va a descontar de irpf segun el sueldo nominal que ingreso
+            return descuento;
+        }
+        }
+
+
+    var franja1 = new Franja(0, (7 * bpc) - 1, 0)
+    var franja2 = new Franja(7 * bpc, (10 * bpc) - 1, 10)
+    var franja3 = new Franja(10 * bpc, (15 * bpc) - 1, 15)
+    var franja4 = new Franja(15 * bpc, (30 * bpc) - 1, 24)
+    var franja5 = new Franja(30 * bpc, (50 * bpc) - 1, 25)
+    var franja6 = new Franja(50 * bpc, (75* bpc) - 1, 27)
+    var franja7 = new Franja(75 * bpc, (115 * bpc) - 1, 31)
+    var franja8 = new Franja(115 * bpc, Number.MAX_VALUE, 36)
+
+    const franjas = [franja1, franja2, franja3, franja4, franja5, franja6, franja7, franja8] 
+    var irpf = new IRPF (franjas)
+    var descuentoirpf = irpf.calculodescuentoIRPF(sueldoNominal)
+
+    if (sueldoNominal > 25 * bpc) descuento_fonasa = (sueldoNominal * aporte_fonasa_desde25bpc) / 100
+    else descuento_fonasa = (sueldoNominal * aporte_fonasa_hasta25bpc) / 100
+    if (sueldoNominal > 15 * bpc) descuentoirpf = (descuentoirpf * tasa_deducciones_desde15bpc) / 100
+    else descuentoirpf = (descuentoirpf * tasa_deducciones_hasta15bpc) / 100
+    return (Math.round (sueldoNominal - descuentoirpf - descuento_jubilatorio - descuento_frl - descuento_fonasa))
 }
 
 function mostrarSueldoLiquido(sueldoLiquido) {
     alert ("Su sueldo liquido es de $" + sueldoLiquido)
 }
+
 
 /** MAIN */
 var sueldoNominal = solicitarSueldoNominal()
